19f5344c334bf075130d214929ff671af4b30ad4
LorenzNacilla <lnacilla31@gmail.com>
2025-11-12 13:46:18 +0000

    putting packages in main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import requests as r
+import time as t
+from datetime import datetime
+import json
+import os
+import boto3
+import sys
+from dotenv import load_dotenv
+
 from bike_point_extract import extract
 from bike_point_load import load
 
